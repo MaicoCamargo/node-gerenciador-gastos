55c167517151f44b8ec7b31da0e07268427839f7
maico.silva@meta.com.br <camargop>
2020-01-08 14:39:49 -0300

    criada rota que cria usuarios

--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
 const app = require('express')();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.status(200).send();
+});
+
+app.get('/users', (req, res) => {
+  const users = [{ nome: 'Maico Camargo' }];
+  res.status(200).send(users);
+});
+
+app.post('/users', (req, res) => {
+  res.status(201).json(req.body);
 });
 
 module.exports = app;
---
+++ test/user.test.js
@@ -0,0 +1,17 @@
+const request = require('supertest');
+const app = require('../src/app');
+
+test('deve listar todos usuarios', () => {
+  return request(app).get('/users').then((response) => {
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveLength(1);
+    expect(response.body[0]).toHaveProperty('nome', 'Maico Camargo');
+  });
+});
+
+test('deve criar um usuario com sucesso', () => {
+  return request(app).post('/users', { nome: 'Eder Camargo' }).then((response) => {
+    expect(response.status).toBe(201);
+    expect(response.body.nome).toBe('Eder Camargo');
+  });
+});
