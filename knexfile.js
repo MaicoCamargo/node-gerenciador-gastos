dba458d21e158397ac246b3ae34d0afa657d9a64
maico.silva@meta.com.br <camargop>
2020-01-12 00:17:54 -0300

    conectando knex a base de dados

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+module.exports = {
+  test: {
+    client: 'postgres',
+    version: '10',
+    connection: {
+      host: '127.0.0.1',
+      user: 'postgres',
+      password: 'Portella08',
+      database: 'gestor-gastos-db',
+    },
+    migrations: { directory: 'src/migrations' },
+
+  },
+};
