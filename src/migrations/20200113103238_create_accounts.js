caa2e3eba784cd314d02f248955570859920953d
maico.silva@meta.com.br <camargop>
2020-01-13 09:52:19 -0300

    criada tabela acconut

---
+++ src/migrations/20200113103238_create_accounts.js
@@ -0,0 +1,12 @@
+
+exports.up = (knex) => {
+  return knex.schema.createTable('account', (table) => {
+    table.increments('id').primary();
+    table.string('name').notNull();
+    table.integer('user_id').references('id').inTable('user').notNull();
+  });
+};
+
+exports.down = (knex) => {
+  return knex.schema.dropTable('account');
+};
