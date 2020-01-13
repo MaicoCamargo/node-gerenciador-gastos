8e8054ebbb0a4fc7fb4552131a9a9100e0dbd678
maico.silva@meta.com.br <camargop>
2020-01-13 09:18:14 -0300

    validando insert de usuario sem senha e email

--- src/services/user.js
+++ src/services/user.js
@@ -5,6 +5,8 @@ module.exports = (app) => {
 
   const save = (user) => {
     if (!user.nome) return { error: 'Nome é um atributo obrigatório' };
+    if (!user.mail) return { error: 'Email é um atributo obrigatório' };
+    if (!user.passwd) return { error: 'Senha é um atributo obrigatório' };
     return app.db('user').insert(user, '*');
   };
 
--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -1,14 +1,14 @@
 const request = require('supertest');
 const app = require('../../src/app');
 
-test('deve listar todos usuarios', () => {
+test('Deve listar todos usuarios', () => {
   return request(app).get('/users').then((response) => {
     expect(response.status).toBe(200);
     expect(response.body.length).toBeGreaterThan(0);
   });
 });
 
-test('deve criar um usuario com sucesso', () => {
+test('Deve criar um usuario com sucesso', () => {
   const mail = `teste_${Date.now()}_@mail.com`;
   return request(app).post('/users').send({ nome: 'Eder Camargo', mail, passwd: '1234' }).then((response) => {
     expect(response.status).toBe(201);
@@ -16,10 +16,34 @@ test('deve criar um usuario com sucesso', () => {
   });
 });
 
-test('não deve criar um usuario sem nome', () => {
+/**
+ * teste usando return
+ */
+test('Não deve criar um usuario sem nome', () => {
   return request(app).post('/users').send({ mail: 'test@email.com', passwd: '1234' })
     .then((response) => {
       expect(response.status).toBe(400);
       expect(response.body.error).toBe('Nome é um atributo obrigatório');
     });
+});
+
+/**
+ * teste usando async await
+ */
+test('Não deve criar um usuario sem email', async () => {
+  const result = await request(app).post('/users').send({ nome: 'ana pa', passwd: '1234' });
+  expect(result.status).toBe(400);
+  expect(result.body.error).toBe('Email é um atributo obrigatório');
+});
+
+/**
+ * não esquecer de usar o done() no final da promisse
+ * - caso der erro pegar no catch e usar done.fail() para sinal que o teste falhou
+ */
+test('Não deve criar um usuario sem senha', (done) => {
+  request(app).post('/users').send({ nome: 'ana pa', mail: 'test@email.com' }).then((result) => {
+    expect(result.status).toBe(400);
+    expect(result.body.error).toBe('Senha é um atributo obrigatório');
+    done();
+  });
 });
