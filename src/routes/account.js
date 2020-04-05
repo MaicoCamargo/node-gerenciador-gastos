f1958fc7a10380d390bab0c7a7feb8d41b68024d
Maico Camargo <maicocamargo08@hotmail.com>
2020-04-05 19:48:17 -0300

    validando erros na aplicação de forma dinamica

--- src/routes/account.js
+++ src/routes/account.js
@@ -1,28 +1,32 @@
 module.exports = (app) => {
-  const findAll = (req, res) => {
-    return app.services.account.find().then((result) => res.status(200).json(result));
+  const findAll = (req, res, next) => {
+    return app.services.account.find()
+      .then((result) => res.status(200).json(result))
+      .catch((error) => next(error));
   };
 
-  const create = (req, res) => {
+  const create = (req, res, next) => {
     app.services.account.save(req.body).then((result) => {
       return res.status(201).json(result[0]);
-    }).catch((err) => {
-      return res.status(400).json({ error: err.message });
-    });
+    }).catch((error) => next(error));
   };
 
-  const findById = (req, res) => {
+  const findById = (req, res, next) => {
     app.services.account.find({ id: req.params.id })
-      .then((result) => res.status(200).json(result[0]));
+      .then((result) => res.status(200).json(result[0]))
+      .catch((error) => next(error));
   };
 
-  const update = (req, res) => {
+  const update = (req, res, next) => {
     app.services.account.update(req.params.id, req.body)
-      .then((result) => res.status(200).json(result[0]));
+      .then((result) => res.status(200).json(result[0]))
+      .catch((error) => next(error));
   };
 
-  const remove = (req, res) => {
-    app.services.account.deletar(req.params.id).then(() => res.status(204).send());
+  const remove = (req, res, next) => {
+    app.services.account.deletar(req.params.id)
+      .then(() => res.status(204).send())
+      .catch((error) => next(error));
   };
 
   return {
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,15 +1,17 @@
 module.exports = (app) => {
-  const findAll = (req, res) => {
-    app.services.user.findAll().then((result) => { res.status(200).json(result); });
+  const findAll = (req, res, next) => {
+    app.services.user.findAll()
+      .then((result) => { res.status(200).json(result); })
+      .catch((error) => next(error));
   };
 
-  const create = async (req, res) => {
+  const create = async (req, res, next) => {
     try {
       const result = await app.services.user.save(req.body);
       if (result.error) { return res.status(400).json(result); }
       return res.status(201).json(result[0]);
     } catch (err) {
-      return res.status(400).json({ error: err.message });
+      return next(err);
     }
   };
 
