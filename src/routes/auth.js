90fa61eae1be4212bc990219ef2a6baaa2cf1e61
Maico Camargo <maicocamargo08@hotmail.com>
2020-04-06 02:37:14 -0300

    Autenticando pelo postman, mas dando erro nos testes

---
+++ src/routes/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jwt-simple');
+const bcrypt = require('bcrypt-nodejs');
+// TODO add o segredi no arquivo .env
+const segredo = 'segredo';
+
+module.exports = (app) => {
+  const signin = async (req, res, next) => {
+    app.services.user.findOne({ mail: req.body.mail })
+      .then((usuario) => {
+
+        if (bcrypt.compareSync(req.body.passwd, usuario.passwd)) {
+          const payload = {
+            id: usuario.id,
+            name: usuario.nome,
+            mail: usuario.mail,
+          };
+          const token = jwt.encode(payload, segredo);
+          res.status(200)
+            .json({token});
+        }
+      })
+      .catch((error) => next(error));
+  };
+
+  return { signin };
+};
--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -4,12 +4,12 @@ const app = require('../../src/app');
 
 test('Deve retornar token ao logar', async () => {
   const mail = `teste_autenticar_${Date.now()}_@mail.com`;
-  const passwd = '1234';
+  const passwd = '123456';
   // criando usuario
   const usuario = app.services.user.save({ nome: 'autenti', mail, passwd });
   expect(usuario.id).not.toBeNull();
   // tentar logar usuario
   const response = await request(app).post('/auth/signin').send({ mail, passwd });
-  expect(response.status).toBe(200);
   expect(response).toHaveProperty('token');
+  expect(response.status).toBe(200);
 });
