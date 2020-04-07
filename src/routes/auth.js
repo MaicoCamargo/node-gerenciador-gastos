98f752a1152de961d4a5fb88c654498e2434bf52
Maico Camargo <maicocamargo08@hotmail.com>
2020-04-07 15:13:08 -0300

    novos testes referentes a autenticação

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,6 @@
 const jwt = require('jwt-simple');
 const bcrypt = require('bcrypt-nodejs');
+const ValidationError = require('../errors/validationError');
 // TODO add o segredi no arquivo .env
 const segredo = 'segredo';
 
@@ -7,6 +8,7 @@ module.exports = (app) => {
   const signin = async (req, res, next) => {
     app.services.user.findOne({ mail: req.body.mail })
       .then((usuario) => {
+        if (!usuario) throw new ValidationError('Login Invalido');
         if (bcrypt.compareSync(req.body.passwd, usuario.passwd)) {
           const payload = {
             id: usuario.id,
@@ -16,7 +18,7 @@ module.exports = (app) => {
           const token = jwt.encode(payload, segredo);
           res.status(200)
             .json({ token });
-        }
+        } else throw new ValidationError('Login Invalido');
       })
       .catch((error) => next(error));
   };
--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../../src/app');
 
 
-test('Deve retornar token ao logar', async () => {
+test('Deve retornar token ao logar com sucesso', async () => {
   const mail = `teste_autenticar_${Date.now()}_@mail.com`;
   const passwd = '123456';
   // criando usuario
@@ -12,4 +12,25 @@ test('Deve retornar token ao logar', async () => {
   const response = await request(app).post('/auth/signin').send({ mail, passwd });
   expect(response.status).toBe(200);
   expect(response.body).toHaveProperty('token');
+});
+
+test('Não Deve logar com usuário ou senha incorretos', async () => {
+  const mail = `teste_autenticar_${Date.now()}_@mail.com`;
+  const passwd = '123456';
+  // criando usuario
+  const usuario = await app.services.user.save({ nome: 'autenti', mail, passwd });
+  expect(usuario.id).not.toBeNull();
+  // tentar logar usuario
+  const response = await request(app).post('/auth/signin').send({ mail, passwd: 'minhasenha' });
+  expect(response.status).toBe(400);
+  expect(response.body.error).toBe('Login Invalido');
+});
+
+test('Não Deve logar com usuário que não existe', async () => {
+  const mail = 'naoexite@mail.com';
+  const passwd = '123456';
+  // tentar logar usuario
+  const response = await request(app).post('/auth/signin').send({ mail, passwd });
+  expect(response.status).toBe(400);
+  expect(response.body.error).toBe('Login Invalido');
 });
