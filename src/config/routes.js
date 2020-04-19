8d46fcb2d0b6ca5d9974187c8064ca788cfc2bc4
Maico Camargo <maicocamargo08@hotmail.com>
2020-04-19 17:11:46 -0300

    bloqueando rotas do sistema

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,14 +3,17 @@ module.exports = (app) => {
     .post(app.routes.auth.signin);
 
   app.route('/users')
+    .all(app.config.passport.authenticate())
     .get(app.routes.users.findAll)
     .post(app.routes.users.create);
 
   app.route('/account')
+    .all(app.config.passport.authenticate())
     .get(app.routes.account.findAll)
     .post(app.routes.account.create);
 
   app.route('/account/:id')
+    .all(app.config.passport.authenticate())
     .get(app.routes.account.findById)
     .put(app.routes.account.update)
     .delete(app.routes.account.remove);
