9670f834694c0e5b0914c061955fbdcca52c2652
Maico Camargo <maicocamargo08@hotmail.com>
2020-04-26 19:47:18 -0300

    add nova table no banco de dados

---
+++ src/migrations/20200426193236_create_table_transaction.js
@@ -0,0 +1,16 @@
+
+exports.up = (knex) => {
+  return knex.schema.createTable('transactions', (table) => {
+    table.increments('id').primary();
+    table.string('descripition').notNull();
+    table.enu('type', ['I', 'O']).notNull();
+    table.date('date').notNull();
+    table.decimal('amnount', 15, 2).notNull();
+    table.boolean('status').notNull().default(false);
+    table.integer('acc_id').references('id').inTable('account').notNull();
+  });
+};
+
+exports.down = (knex) => {
+  return knex.schema.dropTable('transactions');
+};
