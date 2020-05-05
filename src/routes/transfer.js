9237a9c6e7fe2afe6af9696570d7faf8802a0ea1
Maico Camargo <maicocamargo08@hotmail.com>
2020-05-05 17:25:41 -0300

    adicionado middleware para validar o objeto antes de salvar no banco de dados

--- src/routes/transfer.js
+++ src/routes/transfer.js
@@ -3,13 +3,25 @@ const express = require('express');
 module.exports = (app) => {
   const router = express.Router();
 
+  /**
+   * middleware de validação se os objeto da requisição tem todos os campos para ser salvo no banco
+   * @param req - body com o JSON
+   * @param res
+   * @param next
+   */
+  const validation = (req, res, next) => {
+    app.services.transfer.validate({ ...req.body, user_id: req.user.id })
+      .then(() => next())
+      .catch((error) => next(error));
+  };
+
   router.get('/', (req, res, next) => {
     app.services.transfer.find({ user_id: req.user.id })
       .then((result) => res.status(200).json(result))
       .catch((error) => next(error));
   });
 
-  router.post('/', (req, res, next) => {
+  router.post('/', validation, (req, res, next) => {
     app.services.transfer.save({ ...req.body, user_id: req.user.id })
       .then((result) => res.status(201).json(result[0]))
       .catch((error) => next(error));
@@ -21,7 +33,7 @@ module.exports = (app) => {
       .catch((error) => next(error));
   });
 
-  router.put('/:id', (req, res, next) => {
+  router.put('/:id', validation, (req, res, next) => {
     app.services.transfer.update(req.params.id, req.body)
       .then((result) => res.status(200).json(result[0]))
       .catch((error) => next(error));
